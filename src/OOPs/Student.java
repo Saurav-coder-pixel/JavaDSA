57330ad07e5aa12c9ea2a88e962d296798b7ba82
Saurav Kumar <sauravkumar7678g@gmail.com>
2025-08-29 00:32:19 +0530

    Change

--- src/OOPs/Student.java
+++ src/OOPs/Student.java
@@ -2,8 +2,20 @@ package OOPs;
 
 public class Student {
     String name;
-    private int rollNo;
+    int rollNo;
     double percentage;
+
+    //Default constructor
+    public Student(){
+
+    }
+
+    public Student(String name, int rollNo, double percentage){
+        this.name= name;
+        this.rollNo= rollNo;
+        this.percentage= percentage;
+    }
+
 
     public int getRollNo() {
         return rollNo;
--- src/OOPs/StudentClass.java
+++ src/OOPs/StudentClass.java
@@ -2,14 +2,26 @@ package OOPs;
 
 public class StudentClass {
     public static void main(String[] args){
-        Student s1= new Student();
-
-        s1.name= "Saurav";
-        s1.percentage=99.9;
+//        Student s1= new Student();
+//
+//        s1.name= "Saurav";
+//        s1.percentage=99.9;
 //        s1.rollNo=22;
 
 //        System.out.println(s1.getRollNo());
-        s1.setRollNo(33);
-        System.out.println(s1.getRollNo());
+//        s1.setRollNo(33);
+//        System.out.println(s1.getRollNo());
+
+        Student s1= new Student();
+        s1.name= "Saurav";
+        s1.percentage= 99;
+        s1.rollNo=1;
+
+        Student s2= new Student("Sarvesh", 9, 98.9);
+
+        System.out.println(s1.name);
+        System.out.println(s2.name);
+        System.out.println(s1.percentage);
+        System.out.println(s2.rollNo);
     }
 }
